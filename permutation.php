fcf641758fb0182928e3262829f9ec83bd83d9d3
Naohisa Yokoyama <nhsykym@gmail.com>
2020-04-14 20:52:50 +0900

    Remove duplicate output

--- permutation.php
+++ permutation.php
@@ -3,8 +3,10 @@
 $stdin_arr = str_split($argv[1]);
 
 function permutation($array, $prefix = '') {
-  if (count($array) == 1) {
+  static $result = [];
+  if (count($array) == 1 && !in_array($prefix . $array[0], $result)) {
     echo $prefix . $array[0] . "\n";
+    $result[] = $prefix . $array[0];
   } else {
     for ($i = 0; $i < count($array); $i++) {
       $target = array_splice($array, $i, 1);
